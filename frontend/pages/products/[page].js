312c65938baad5f7c25d21e5935ddf7d44f35630
snyles <sniles@gmail.com>
2021-06-23 11:28:49 -0400

    adjust products query

--- frontend/pages/products/[page].js
+++ frontend/pages/products/[page].js
@@ -1,15 +1,1 @@
-import { useRouter } from 'next/router';
-import Products from '../../components/Products';
-import Pagination from '../../components/Pagination';
-
-export default function ProductsPage() {
-  const { query } = useRouter();
-  const page = parseInt(query.page);
-  return (
-    <div>
-      <Pagination page={page || 1} />
-      <Products />
-      <Pagination page={page || 1} />
-    </div>
-  );
-}
+export { default } from './index';
